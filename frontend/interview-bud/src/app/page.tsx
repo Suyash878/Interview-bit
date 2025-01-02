d62d8ce65432bf3a0a6e44302b144929eeb850f6
Suyash878 <suyashmishra145@gmail.com>
2025-01-02 15:30:06 +0530

    Adding the final setup and also removing .env from the codebase

--- frontend/interview-bud/src/app/page.tsx
+++ frontend/interview-bud/src/app/page.tsx
@@ -1,9 +1,51 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
+import ReactMarkdown from 'react-markdown';
+import axios from "axios";
 
 export default function Home() {
+
+  const [question, setQuestion] = useState<string[]>([]);
+  const [resultPrompt, setResultPrompt] = useState<string[]>([]);
+
+  async function handleClick() {  
+    const url = 'http://localhost:3000/generate';
+    const data = {  
+      question: question
+    }
+    try 
+    {   
+      const response = await axios.post(url, data);
+      setResultPrompt([...resultPrompt, JSON.stringify(response.data.response)]);
+    }
+    catch(err) 
+    {
+        console.log('Some error occurred'+ err);
+    }
+  }
+
   return (
-    <div className="">
-      hi this is next app
+    <div className="p-4 text-black">
+        <div className="gap-4">
+            <input type="text" onChange={(e) => 
+              {
+                setQuestion([...question, e.target.value]);
+              }
+            } value={question}  placeholder="Enter your question"/>
+            <button className="text-white" onClick={handleClick}>Send</button>
+            <div className="text-white">
+              {
+                resultPrompt.map((prompt, index) => {
+                  return (
+                    <div key={index}>
+                      You: 
+                      <ReactMarkdown>{prompt}</ReactMarkdown>
+                    </div>
+                  )
+                })
+              }
+            </div>
+        </div>
     </div>
   );
 }
